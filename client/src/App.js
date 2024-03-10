1547e6c91e86e35cd0b25472fd09370db1cf79b8
Jaweria B <bjaweria509@gmail.com>
2024-03-10 14:25:48 +0500

    added home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Fragment } from 'react';
+
 import './App.css';
+
+import Home from './routes/home/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Fragment>
+        <Routes>
+          {/* <Route path='/' element={<Navigation />}> */}
+          <Route index element={<Home/>} />
+        </Routes>
+      </Fragment>
     </div>
   );
 }
