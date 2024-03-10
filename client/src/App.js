9e506914b6aa012cfa660535fba388c92d7d5e19
Jaweria B <bjaweria509@gmail.com>
2024-03-10 15:08:09 +0500

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,15 @@ import { Fragment } from 'react';
 import './App.css';
 
 import Home from './routes/home/home';
+import Navigation from './routes/navigation/navigation';
 
 function App() {
   return (
     <div>
       <Fragment>
+        <Navigation/>
         <Routes>
-          {/* <Route path='/' element={<Navigation />}> */}
+          <Route path='/' element={<Navigation />} />
           <Route index element={<Home/>} />
         </Routes>
       </Fragment>
---
+++ client/src/routes/navigation/navigation.jsx
@@ -0,0 +1,50 @@
+const Navigation = () => {
+    return (
+        <nav class="navbar navbar-expand-lg bg-white shadow-lg">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <a class="navbar-brand" href="index.html">
+                    Book Shelf
+                </a>
+
+                <div class="d-lg-none">
+                    <button type="button" class="custom-btn btn btn-danger" data-bs-toggle="modal" data-bs-target="#BookingModal">Reservation</button>
+                </div>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Story</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Don't know</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Our Updates</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="d-none d-lg-block">
+                    <button type="button" class="custom-btn btn btn-danger" data-bs-toggle="modal" data-bs-target="#BookingModal">Reservation</button>
+                </div>
+
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
